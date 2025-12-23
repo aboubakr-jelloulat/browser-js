68b1022158c0d8d32a649ad35d1f13dcfcc7c8b5
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-12-23 16:37:31 +0100

    Comparison

---
+++ fund/Comparison.js
@@ -0,0 +1,13 @@
+// console.log("Hello" + " World");
+// console.log(!true); 
+
+
+// Equal(==)  check the values are equal
+console.log(44 == "44");
+
+// Equal (===) Strict check the value and the type the both
+console.log(44 === "44")
+console.log(44 === 44)
+console.log(44 === 42) 
+
+
