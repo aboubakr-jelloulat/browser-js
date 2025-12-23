3d499671d247434fc2941900cf88b2db9372f497
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-12-23 17:04:27 +0100

    Comparison InEquality

--- fund/Comparison.js
+++ fund/Comparison.js
@@ -1,13 +1,25 @@
-// console.log("Hello" + " World");
-// console.log(!true); 
+console.log("*** + Comparison ***");
+console.log("Hello" + " World");
+console.log(!true); 
 
 
+
+console.log("*** + Equality ***");
 // Equal(==)  check the values are equal
 console.log(44 == "44");
-
 // Equal (===) Strict check the value and the type the both
 console.log(44 === "44")
 console.log(44 === 44)
 console.log(44 === 42) 
+
+
+
+console.log("*** + InEquality ***");
+// (==)
+console.log(42 != "42");
+console.log("42" != "1337");
+// (===)
+console.log("42" !== 19);
+console.log(42 !== 1337);
 
 
