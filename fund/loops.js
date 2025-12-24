8f74444f83d3858648041b3bca17d2fd2f1d0fe2
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-12-24 16:48:22 +0100

    loops

---
+++ fund/loops.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+
+function fill_array_and_print_it() 
+{
+    let n = Number(prompt("Enter the number of elements: "));
+    let arr = [];
+
+    for (let i = 0; i < n; i++) 
+        arr[i] = Number(prompt(`arr[${i}] = `));
+    
+
+    console.log("\nArray Elements:");
+    for (let i = 0; i < n; i++) 
+        console.log(arr[i]);
+    
+}
+
+fill_array_and_print_it();
