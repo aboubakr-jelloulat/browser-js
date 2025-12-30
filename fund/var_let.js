0f6b7cbb30394205265e798266921d7053d91107
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-12-30 17:59:14 +0100

    var vs let

---
+++ fund/var_let.js
@@ -0,0 +1,45 @@
+
+
+if (true)
+{
+	var x = 1337;
+}
+console.log(x); //  1337  var ignores { } blocks like if, for, while
+
+
+function foo()
+{
+	var nb = 42;
+}
+// console.log(nb); // ERROR you can't 
+
+
+if (true)
+{
+	let a = 19;
+}
+// console.log(a);  // you can't using let !!!
+
+
+
+const a = 42;
+// a = 1337; // you can't modify
+
+
+
+function example() 
+{
+	if (true) 
+	{
+	  var x = 1337;
+	  let y = 42;
+	  const z = 19;
+	}
+  
+	console.log(x); //  works
+	// console.log(y); // error
+	// console.log(z); //  error
+}
+example();
+
+
