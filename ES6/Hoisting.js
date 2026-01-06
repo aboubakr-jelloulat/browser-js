1c343b5cb18daf73dcc83c9e7f0afdab1261e977
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-01-06 09:39:55 +0100

    Hoisting

--- ES6/Hoisting.js
+++ ES6/Hoisting.js
@@ -1,5 +1,14 @@
+/*
+What is Hoisting?
+	Hoisting means that JavaScript moves declarations to the top of their scope before the code runs.
+
+Important:
+	Only declarations are hoisted
+	Initializations are NOT hoisted
+*/
+
 
 console.log(name);
 var name = "ajelloul";
 
-// Hoisting  26:40
+
