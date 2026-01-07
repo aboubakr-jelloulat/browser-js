37061fa26995d7ed2765de8f22ad5b6816861748
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-01-07 22:18:44 +0100

    Template Literals

---
+++ ES6/TemplateLiterals.js
@@ -0,0 +1,14 @@
+
+// substitutions : ${variable_name}
+let firstName = 'ajelloul', nickname = 'borz';
+let greeting = `Hej ${firstName}, ${nickname}`;
+
+console.log(greeting);
+
+
+// break line
+console.log("hello\nhej");
+const message = `This has to be 
+	on two line`;
+
+console.log(message);
